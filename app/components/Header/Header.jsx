5c0523bcb6a4c431bb04de25026b4971cd5539d1
Jeff Goodall <goodallhj@gmail.com>
2018-02-06 16:09:11 -0700

    Adding reducers.js and placing it in index.js

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// needs to be a redux container to keep state of the sign in input to show favorties when signed in or "sign in" when not.
 const Header = () => {
   return (
     <div>
-      <h1>Movie Watcher</h1>
+      <h1>Movie Tracker, YO!</h1>
+      {/* <navLink path={/}></navLink> */}
     </div>
   );
 };
--- app/index.js
+++ app/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 import MovieIndex from './components/movieIndex';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/reducers'
 
+const store = createStore(rootReducer)
 const router = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
 
-
-ReactDOM.render(router, document.getElementById('main'));
+ReactDOM.render(
+  <Provider store={store}>router</Provider>,
+  document.getElementById('main')
+);
